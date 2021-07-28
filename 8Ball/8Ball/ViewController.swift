a2010997d51444e3d6baf902d890eadcb2ad9480
Vamsi Pachamatla <vamsiraju835@gmail.com>
2021-07-28 12:31:37 +0530

    Finished the selection of random images when button is pressed

--- 8Ball/8Ball/ViewController.swift
+++ 8Ball/8Ball/ViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var eightBallImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        eightBallImage.image = #imageLiteral(resourceName: "ball4")
         
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        eightBallImage.image = [#imageLiteral(resourceName: "ball3") , #imageLiteral(resourceName: "ball4") , #imageLiteral(resourceName: "ball2") , #imageLiteral(resourceName: "ball4") , #imageLiteral(resourceName: "ball2")][Int.random(in: 0...4)]
+    }
+    
 }
 
